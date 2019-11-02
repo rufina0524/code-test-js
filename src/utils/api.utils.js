38569e840a6c8c85cee6d63fcd9439413f891f31
Rufina Tsoi <rufina0524@gmail.com>
2019-11-02 21:56:50 +0000

    Add fourSquare API.

---
+++ src/utils/api.utils.js
@@ -0,0 +1,25 @@
+const clientId = 'UWNGMI23BN1DU0F4C2V4NV4Y1LYOOH4SPFFQH5VIH31ZPEL0'
+const clientSecret = 'GYMSBI4CSUSX0XTTFOH54KVVL51M1WED11XMKW5QRCLEN15I'
+const baseUrl = 'https://api.foursquare.com/v2/venues'
+
+const request = async (url, query = []) => {
+  const qString = query.concat([
+    `client_id=${clientId}`,
+    `client_secret=${clientSecret}`,
+    'v=20191102'
+  ]).join('&')
+
+  const response = await fetch(`${url}?${qString}`)
+  return response.json()
+}
+
+export const getVenueNearLondon = async (venurName) => {
+  const data = await request(
+    `${baseUrl}/search`,
+    [
+      'near=London',
+      'query=Satori'
+    ]
+  )
+  console.log(data.response);
+}
