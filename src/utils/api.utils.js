1876f16ff8cf9283c5bf7ff00500e93e255e6bcd
Rufina Tsoi <rufina0524@gmail.com>
2019-11-09 22:29:19 +0000

    Use auth token.

--- src/utils/api.utils.js
+++ src/utils/api.utils.js
@@ -1,44 +1,41 @@
 import _isObject from 'lodash/isObject';
+import moment from 'moment';
 
-const defaultClientId = 'UWNGMI23BN1DU0F4C2V4NV4Y1LYOOH4SPFFQH5VIH31ZPEL0'
-const defaultClientSecret = 'GYMSBI4CSUSX0XTTFOH54KVVL51M1WED11XMKW5QRCLEN15I'
 const baseUrl = 'https://api.foursquare.com/v2/venues'
 
 const request = async (url, query = []) => {
   const qString = query.concat([
-    'v=20191102'
+    `v=${moment().format('YYYYMMDD')}`
   ]).join('&')
 
   const response = await fetch(`${url}?${qString}`)
   return response.json()
 }
 
-export const getVenueNearLondon = async (input) => {
-  if (_isObject(input)) {
-    const { clientId, clientSecret, venueName = '' } = input;
-  
+export const getVenueNearLondon = async (params) => {
+  if (_isObject(params)) {
+    const { authToken, venueName = '' } = params;
+
     const data = await request(
       `${baseUrl}/search`,
       [
         'near=London',
-        `client_id=${clientId || defaultClientId}`,
-        `client_secret=${clientSecret || defaultClientSecret}`,
+        `oauth_token=${authToken}`,
         `query=${venueName}`
       ]
-    )
+    );
     return data.response;
   }
 }
 
 export const getSimilarVenue = async (params) => {
   if (_isObject(params)) {
-    const { clientId, clientSecret, venueId = '' } = params;
+    const { authToken, venueId = '' } = params;
     
     const data = await request(
       `${baseUrl}/${venueId}/similar`,
       [
-        `client_id=${clientId || defaultClientId}`,
-        `client_secret=${clientSecret || defaultClientSecret}`
+        `oauth_token=${authToken}`
       ]
     );
     return data.response;
